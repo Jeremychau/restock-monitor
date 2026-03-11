ac1ab1cee86bacaf7945413e9e1fa13c4c8a85c9
JeremyChau <jeremychau0605@outlook.com>
2026-03-11 17:02:50 +0800

    update

--- index.py
+++ index.py
@@ -6,17 +6,20 @@ URL = os.environ["PRODUCT_URL"]
 TG_TOKEN = os.environ["TG_TOKEN"]
 CHAT_ID = os.environ["TG_CHAT_ID"]
 
-while True:
-    r = requests.get(URL)
-
-    if "Add to cart" in r.text:
-        print("RESTOCK!")
-
-    # time.sleep(60)
-
-
 def send_tg(msg):
     requests.post(
         f"https://api.telegram.org/botTOKEN/sendMessage",
         data={"chat_id":CHATID,"text":msg}
     )
+
+send_tg('hihi')
+
+# while True:
+#     r = requests.get(URL)
+
+#     if "Add to cart" in r.text:
+#         print("RESTOCK!")
+
+    # time.sleep(60)
+
+
